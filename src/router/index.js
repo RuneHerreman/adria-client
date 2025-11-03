da1d6db5593fc0ea2d642dc719a1dcccaa2380c3
adrian.bilici <adrian.bilici@student.howest.be>
2025-11-03 15:23:52 +0100

    all routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,50 @@
 import {createRouter, createWebHistory} from "vue-router";
 import LandingPage from "@/pages/LandingPage.vue";
+import CoursePage from "@/pages/CoursePage.vue";
+import DashboardPage from "@/pages/DashboardPage.vue";
+import LeaderboardPage from "@/pages/LeaderboardPage.vue";
+import LearningCoursePage from "@/pages/LearningCoursePage.vue";
+import OnboardingPage from "@/pages/OnboardingPage.vue";
+import SettingsPage from "@/pages/SettingsPage.vue";
+import ShopPage from "@/pages/ShopPage.vue";
+import SubscriptionPage from "@/pages/SubscriptionPage.vue";
 
 const routes = [
     {
         path: "/",
         component: LandingPage
+    },
+    {
+        path: "/course",
+        component: CoursePage
+    },
+    {
+        path: "/dashboard",
+        component: DashboardPage
+    },
+    {
+        path: "/leaderboard",
+        component: LeaderboardPage
+    },
+    {
+        path: "/learning-course",
+        component: LearningCoursePage
+    },
+    {
+        path: "/onboarding",
+        component: OnboardingPage
+    },
+    {
+        path: "/settings",
+        component: SettingsPage
+    },
+    {
+        path: "/shop",
+        component: ShopPage
+    },
+    {
+        path: "/subscription",
+        component: SubscriptionPage
     }
 ];
 
