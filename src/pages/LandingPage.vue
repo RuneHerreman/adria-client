99079af1363066cb0928241a6fee034e1dd3b5bd
adrian.bilici <adrian.bilici@student.howest.be>
2025-11-03 15:17:23 +0100

    open website on LandingPage.vue

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -3,7 +3,10 @@
 </script>
 
 <template>
-
+  <div class="landing-page">
+    <h1>Welcome to the Landing Page</h1>
+    <p>This is the main entry point of the application.</p>
+  </div>
 </template>
 
 <style scoped>
