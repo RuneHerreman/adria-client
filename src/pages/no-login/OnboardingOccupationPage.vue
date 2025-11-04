136cff372a48b316ae0e57f9cf2705ea30460422
adrian.bilici <adrian.bilici@student.howest.be>
2025-11-04 15:56:24 +0100

    text

--- src/pages/no-login/OnboardingOccupationPage.vue
+++ src/pages/no-login/OnboardingOccupationPage.vue
@@ -6,6 +6,12 @@ import GenesisLogo from "@/assets/media/genesis.png";
   <div class="onboarding-container">
     <div class="onboarding-card">
       <img :src="GenesisLogo" alt="Genesis logo" class="logo" />
+      <p class="welcome-text">Welcome to</p>
+
+      <p class="subtitle">
+        Let’s personalize your learning experience.<br />
+        What’s your current occupation?
+      </p>
     </div>
   </div>
 </template>
