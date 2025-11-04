fc85848f8c6a05de47c2ddaa95d666be74303bfb
adrian.bilici <adrian.bilici@student.howest.be>
2025-11-04 09:43:23 +0100

    pictures added

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,10 +1,16 @@
 <script setup>
-
+import GenesisLogo from "@/assets/media/Genesis.png";
+import InkeaLogo from "@/assets/media/INKEA.png";
 </script>
 
 <template>
   <footer>
-
+    <div id="content">
+      <div id="logos">
+        <img :src="GenesisLogo" alt="Genesis Logo" class="logo" />
+        <img :src="InkeaLogo" alt="Inkea Logo" class="logo" />
+      </div>
+    </div>
   </footer>
 </template>
 
