b096de9110082c32253a19f5a9ffbe6bd2fccfdf
adrian.bilici <adrian.bilici@student.howest.be>
2025-11-04 15:55:21 +0100

    image added

---
+++ src/pages/no-login/OnboardingOccupationPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import GenesisLogo from "@/assets/media/genesis.png";
+</script>
+
+<template>
+  <div class="onboarding-container">
+    <div class="onboarding-card">
+      <img :src="GenesisLogo" alt="Genesis logo" class="logo" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
