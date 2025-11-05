584d055467c158fa4c612b22eb3bd19abb0e0830
adrian.bilici <adrian.bilici@student.howest.be>
2025-11-05 10:38:42 +0100

    create the store

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import './assets/css/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router/index.js";
 import "./assets/css/reset.css";
 import "./assets/css/main.css";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
 app.use(router)
-    .mount('#app')
+    .use(createPinia())
+    .mount('#app');
