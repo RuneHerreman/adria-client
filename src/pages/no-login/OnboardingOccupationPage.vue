2f0a40cc35e64db626bf71ed27048ebe41fb9693
adrian.bilici <adrian.bilici@student.howest.be>
2025-11-05 09:26:51 +0100

    perfect html

--- src/pages/no-login/OnboardingOccupationPage.vue
+++ src/pages/no-login/OnboardingOccupationPage.vue
@@ -3,36 +3,32 @@ import GenesisLogo from "@/assets/media/genesis.png";
 </script>
 
 <template>
-  <main>
-    <section class="onboarding-centered-section">
-      <div class="onboarding-info-block">
-        <small class="onboarding-small-text">Welcome to</small>
-        <img :src="GenesisLogo" alt="Genesis Logo" class="logo" />
-        <small class="onboarding-small-text">
-          Let’s personalize your learning experience.<br />
-          What’s your current occupation?
-        </small>
-      </div>
+  <section class="onboarding-centered-section">
+    <div class="onboarding-info-block">
+      <small class="onboarding-small-text">Welcome to</small>
+      <img :src="GenesisLogo" alt="Genesis Logo" class="logo" />
+      <small class="onboarding-small-text">
+        Let’s personalize your learning experience.<br />
+        What’s your current occupation?
+      </small>
+    </div>
 
-      <div class="onboarding-input-block">
-        <label for="occupation" class="onboarding-label">Your occupation</label>
-        <input
-            id="occupation"
-            type="text"
-            placeholder="Type your occupation"
-            class="onboarding-input"
-        />
-        <button class="onboarding-btn">Continue</button>
-      </div>
-    </section>
-  </main>
+    <div class="onboarding-input-block">
+      <label for="occupation" class="onboarding-label">Your occupation</label>
+      <input
+          id="occupation"
+          type="text"
+          placeholder="Type your occupation"
+          class="onboarding-input"
+      />
+      <button class="onboarding-btn">Continue</button>
+    </div>
+  </section>
 </template>
 
 <style scoped>
 .logo {
-  width: 12rem;
-  margin: 0.5rem 0;
+  width: 8rem;
+  margin: 0.3rem 0;
 }
-
-
 </style>
