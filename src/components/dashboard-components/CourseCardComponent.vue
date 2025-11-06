96e165e03530503cf54ee28089efd33c9348882b
RuneHerreman <herreman.rune@gmail.com>
2025-11-06 10:26:39 +0100

    Completed course card styling

--- src/components/dashboard-components/CourseCardComponent.vue
+++ src/components/dashboard-components/CourseCardComponent.vue
@@ -1,5 +1,6 @@
 <script setup>
   import DifficultyComponent from "@/components/dashboard-components/DifficultyComponent.vue";
+  import ProgressComponent from "@/components/dashboard-components/ProgressComponent.vue";
 
   const props = defineProps({
     name: {type: String, required: true},
@@ -13,24 +14,93 @@
   <article
       class="course-card"
       :class="{enrolledCourse: enrolled}"
-      :style="{ backgroundImage: `url(../../assets/media/${name}.png)` }"
+      :style="{ background: `url(../../assets/media/course-background/${name}.jpg);`}"
   >
-    <p>{{name}}</p>
-    <DifficultyComponent :difficulty="difficulty"></DifficultyComponent>
+    <div class="course-wrapper">
+      <p class="course-name">{{name}}</p>
+      <div
+          class="course-info"
+      >
+        <DifficultyComponent :difficulty="difficulty"></DifficultyComponent>
+        <div v-if="enrolled=true" class="progress-container">
+          <ProgressComponent progress="50"></ProgressComponent>
+          <p class="progress-percentage">{{completion}}%</p>
+        </div>
+      </div>
+    </div>
   </article>
 </template>
 
 <style scoped>
 article{
+  position: relative;
+
   box-sizing: border-box;
   margin-bottom: 1rem;
   padding: 1rem;
-  height: 6rem;
+  height: 7.5rem;
   width: auto;
   aspect-ratio: 30/13;
 
   border: #e6e6e6 solid 0.0612rem;
   box-shadow: 0 0 0.25rem rgba(217, 217, 217, 0.25);
   border-radius: 0.5rem;
+
+  background: url("../../assets/media/course-background/Bird Blindness - A deepdive.jpg") no-repeat center right;
+  background-size: contain;
+
+  cursor: pointer;
 }
+
+.course-wrapper {
+  position: relative;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  z-index: 2;
+}
+
+article::after{
+  content: "";
+  display: block;
+  width: 100%;
+  height: 100%;
+
+  position: absolute;
+  inset: 0;
+
+  border-radius: 0.5rem;
+  background: linear-gradient(to right, white 60%, transparent 80%);
+  z-index: 1;
+}
+
+.course-name {
+  width: 60%;
+  white-space: wrap;
+  margin-bottom: 1rem;
+  font-size: 1.2rem;
+  line-height: 1.2rem;
+}
+
+.progress-container {
+  display: flex;
+  align-content: center;
+  gap: 0.5rem;
+}
+
+.progress-percentage{
+  color: white;
+  font-weight: 500;
+  font-size: 0.8rem;
+}
+
+.course-info {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+
+
 </style>
---
+++ src/components/dashboard-components/ProgressComponent.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps({
+  progress: {type: Number, required: true},
+})
+</script>
+
+<template>
+  <div class="progress-wrapper">
+    <div :style="{ width: progress + '%' }" class="progress"></div>
+  </div>
+</template>
+
+<style scoped>
+  .progress-wrapper {
+    height: 0.4rem;
+    width: 90%;
+    padding: 0.15rem;
+    background-color: #f2f2f2;
+    border-radius: 0.5rem;
+  }
+  .progress {
+    height: 100%;
+    background-color: var(--bright-green);
+    border-radius: 0.5rem;
+  }
+</style>
