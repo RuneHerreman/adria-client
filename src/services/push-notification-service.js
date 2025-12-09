804e622cad0e598ceb34b9f350f9040ea1e1622d
adrian.bilici <adrian.bilici@student.howest.be>
2025-12-09 08:52:24 +0100

    services added

---
+++ src/services/push-notification-service.js
@@ -0,0 +1,46 @@
+
+const VAPID_PUBLIC_KEY = "BPd94pRlGUtPWA9aEjwjcADqLuE7bdevk-IzxDqJnPx4LlRVZgkJPHq2-GICOA833uonMxnFc8pNCZjyUiy-ePY";
+
+const subscripteOptions = {
+  userVisibleOnly: true,
+  applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY)
+}
+
+async function enablePushNotifications() {
+  const permission = await Notification.requestPermission();
+  if (permission === "granted") renewPushNotification();
+}
+
+async function renewPushNotification() {
+  const registration = await navigator.serviceWorker.ready;
+  const subscription = await registration.pushManager.subscribe(subscripteOptions);
+
+  postSubscriptionToServer(subscription);
+}
+
+function postSubscriptionToServer(subscription) {
+  fetch("http://localhost:8000/subscribe", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(subscription)
+  });
+}
+
+function urlBase64ToUint8Array(base64String) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+
+export { enablePushNotifications };
