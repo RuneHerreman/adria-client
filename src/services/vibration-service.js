9b79f6959bf04aaad6d0885cb22a23dfc7738881
Onebamoi Steven <steven.onebamoi@student.howest.be>
2025-12-16 11:12:36 +0100

    Finished Vibration api

--- src/services/vibration-service.js
+++ src/services/vibration-service.js
@@ -1,6 +1,8 @@
-function vibrate(pattern) {
-  if ("vibrate" in navigator)
+export function vibrate(pattern) {
+  if ("vibrate" in navigator) {
     navigator.vibrate(pattern);
+  } else {
+    console.warn("Vibration API not supported");
+  }
 }
 
-export { vibrate }
